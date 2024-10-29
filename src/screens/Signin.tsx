840194d219970d1e9d404c1c048505aa4f26bbd5
danilocucharro <danicucharro@outlook.com>
2024-10-29 09:45:09 -0300

    first commit

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Text, VStack } from "@gluestack-ui/themed";
+import { Center, Heading, Image, Text, VStack } from "@gluestack-ui/themed";
 
 import BackGroundImg from "@assets/background.png"
 import Logo from "@assets/logo.svg"
@@ -21,6 +21,10 @@ export function Signin() {
         <Text color="$gray100" fontSize="$sm">
           Treine sua mente e o seu corpo.
         </Text>
+      </Center>
+
+      <Center>
+        <Heading color="$gray100">Acesse a conta</Heading>
       </Center>
     </VStack>
   )
