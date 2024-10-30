9378757272c68f5d0c37a06fe644120a630dfb38
danilocucharro <danicucharro@outlook.com>
2024-10-30 11:33:50 -0300

    vai fazer o novo componente de card dos exercicios e estilizacao da tela home completa

---
+++ src/screens/Home/components/ExerciseCard.tsx
@@ -0,0 +1,39 @@
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+import { Heading, HStack, Image, VStack, Text, Icon } from "@gluestack-ui/themed";
+
+import { ChevronRight } from "lucide-react-native"
+
+type ExerciseCardProps = TouchableOpacityProps
+
+export function ExerciseCard({ ...rest }: ExerciseCardProps) {
+  return(
+    <TouchableOpacity {...rest}>
+      <HStack bg="$gray500" alignItems="center" p="$2" pr="$4" rounded="$md" mb="$3">
+        <Image 
+          source={{
+            uri: "https://i.pinimg.com/236x/26/a4/31/26a4312ea5c9d9ca5415cef6670f88f0.jpg"
+          }} 
+          alt="exercise example picture"
+          w="$16"
+          h="$16"
+          rounded="$md"
+          mr="$4"
+          resizeMode="cover"
+        />
+
+        <VStack flex={1}>
+          <Heading fontSize="$lg" color="white" fontFamily="$heading">
+            Puxada frontal
+          </Heading>
+
+          <Text fontSize="$sm" color="$gray200" mt="$1" numberOfLines={2}>
+            3 séries x 12 repetições
+          </Text>
+        </VStack>
+
+        <Icon  as={ChevronRight} color="$gray300"/>
+      </HStack>
+    </TouchableOpacity>
+  )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
-import { FlatList } from "@gluestack-ui/themed";
+import { FlatList, Text } from "@gluestack-ui/themed";
 
-import { HStack, VStack } from "@gluestack-ui/themed";
+import { HStack, VStack, Heading } from "@gluestack-ui/themed";
 
 import { Header } from "./components/Header";
 import { MuscleGroup } from "./components/MuscleGroup";
+import { ExerciseCard } from "./components/ExerciseCard";
 
 export function Home() {
   const [muscleGroupList, setMuscleGroupList] = useState([
     "Costas",
     "Ombro",
-    "Biceps",
-    "Triceps"
+    "Bíceps",
+    "Tríceps"
+  ])
+  const [exercisesList, setExercisesList] = useState([
+    "Puxada frontal",
+    "Remada curvada",
+    "Remada unilateral",
+    "levantamento terra",
+    "3",
+    "4",
+    "5"
   ])
   const [groupSelected, setGroupSelected] = useState("Costas")
 
@@ -21,11 +31,11 @@ export function Home() {
 
       <FlatList 
         data={muscleGroupList}
-        keyExtractor={(item) => item}
+        keyExtractor={item => item}
         renderItem={({ item }) => (
           <MuscleGroup 
             name={item}
-            isActive={groupSelected === item} 
+            isActive={groupSelected.toLowerCase() === item.toLowerCase()} 
             onPress={() => setGroupSelected(item)}
           />
         )}
@@ -37,7 +47,27 @@ export function Home() {
           maxHeight: 44,
           minHeight: 44
         }}
-      />     
+      />
+
+      <VStack px="$8" flex={1}>
+        <HStack justifyContent="space-between" mb="$5" alignItems="center">
+          <Heading color="$gray200" fontSize="$md" fontFamily="$heading">
+            Exercícios
+          </Heading>
+
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            {exercisesList.length}
+          </Text>
+        </HStack>
+        
+        <FlatList 
+          data={exercisesList}
+          keyExtractor={item => item}
+          renderItem={() => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
     </VStack>
   )
 }
