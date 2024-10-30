f94aed4fd37e927d312457527a6412f42648deab
danilocucharro <danicucharro@outlook.com>
2024-10-30 10:14:04 -0300

    feat: vai fazer a lista de grupos musculares na tela home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,43 @@
-import { Center, VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
+import { FlatList } from "@gluestack-ui/themed";
+
+import { HStack, VStack } from "@gluestack-ui/themed";
 
 import { Header } from "./components/Header";
+import { MuscleGroup } from "./components/MuscleGroup";
 
 export function Home() {
+  const [muscleGroupList, setMuscleGroupList] = useState([
+    "Costas",
+    "Ombro",
+    "Biceps",
+    "Triceps"
+  ])
+  const [groupSelected, setGroupSelected] = useState("Costas")
+
   return(
     <VStack flex={1}>
       <Header />
+
+      <FlatList 
+        data={muscleGroupList}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <MuscleGroup 
+            name={item}
+            isActive={groupSelected === item} 
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 32 }}
+        style={{
+          marginVertical: 40,
+          maxHeight: 44,
+          minHeight: 44
+        }}
+      />     
     </VStack>
   )
 }
