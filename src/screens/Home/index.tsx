cb96286c45894650f12153f5906744d197229fea
danilocucharro <danicucharro@outlook.com>
2024-11-04 13:22:01 -0300

    feat: faz os grupos musculares serem carregados atraves do banco de dados ao inves de ser um estado estatico

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,24 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { FlatList } from "react-native";
 
-import { Text } from "@gluestack-ui/themed";
-
-import { HStack, VStack } from "@gluestack-ui/themed";
+import { Text, useToast, HStack, VStack } from "@gluestack-ui/themed";
 
 import { Header } from "./components/Header";
 import { MuscleGroup } from "./components/MuscleGroup";
 import { ExerciseCard } from "./components/ExerciseCard";
+import { ToastMessage } from "@components/ToastMessage";
 
 import { AppNavigationRoutesProps } from "@routes/app.routes";
+import { AppError } from "@utils/AppError";
+
+import { api } from "@services/api";
 
 export function Home() {
-  const [muscleGroupList, setMuscleGroupList] = useState([
-    "Costas",
-    "Ombro",
-    "Bíceps",
-    "Tríceps"
-  ])
+  const [muscleGroupList, setMuscleGroupList] = useState<string[]>()
   const [exercisesList, setExercisesList] = useState([
     "Puxada frontal",
     "Remada curvada",
@@ -26,12 +23,39 @@ export function Home() {
     "levantamento terra",
   ])
   const [groupSelected, setGroupSelected] = useState("Costas")
+  const toast = useToast()
 
   const navigation = useNavigation<AppNavigationRoutesProps>()
 
   function handleOpenExerciseInfo() {
     navigation.navigate("exercise")
   }
+
+  async function fetchGroups() {
+    try {
+      const response = await api.get('/groups')
+      setMuscleGroupList(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const errorTitle = isAppError ? error.message : 'Não foi possivel carregar os grupos musculares.'
+
+      toast.show({
+        placement: "top",
+        render: ({ id }) => (
+          <ToastMessage 
+            id={id}
+            title={errorTitle}
+            action="error"
+            onClose={() => toast.close(id)}
+          />
+        )
+      })
+    }
+  }
+
+  useEffect(() => {
+    fetchGroups()
+  }, [])
 
   return(
     <VStack flex={1}>
